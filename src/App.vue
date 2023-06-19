5e5838e4b7ae72a30c515b27a9a43a3cf607edaa
NanaNiki <nicol.wesolowska@gmail.com>
2023-06-19 17:38:08 +0200

    "skeleton, logo, bit of styles"

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Start from './components/Start.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="/">
+      <img src="/skysuplogo.svg" class="logo" alt="Sky Support logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Start msg="Vite + Vue" />
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
+  height: 3em;
+  padding: 1em;
   will-change: filter;
   transition: filter 300ms;
+  position: absolute;
+  top: 1em;
+  left: 2em;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 1em #4ca9fbaa);
 }
 </style>
---
+++ src/components/Start.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Sky Support</h1>
+  <h2>Welcome to Your personal Sky Support</h2>
+  <h3>Space where you can get inspiration and enhance your focus</h3>
+  <h3>Choose you sky:</h3>
+  <input placeholder="Type in location of your dream sky"/>
+  <div>  
+  <button>Random sky</button>
+  <button>Sky near me</button>
+</div>
+</template>
+
+<style scoped>
+input {
+  text-align: center;
+  margin: 1em;
+  border-radius: 8px;
+  border: 1px solid transparent;
+  width: 20em;
+  height: 2em;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+}
+div {
+  flex: auto;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
